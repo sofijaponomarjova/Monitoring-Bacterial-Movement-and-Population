25cd72132b1629ae62a3efc7254d380f080d1928
Sofija Ponomarjova <sofija.ponomarjova@gmail.com>
2026-09-16 12:06:43 +0200

    Modified user input

--- task2.py
+++ task2.py
@@ -1,6 +1,22 @@
 from math import sqrt #imports sqrt function from math library
 import os #library to create files etc.
 
+bacterial_id={
+    "211":"E. coli WT", 
+    "-211":"E. coli mutant", 
+    "321":"Bacillus subtilis WT", 
+    "-321":"Bacillus subtilis mutant", 
+    "2212":"Pseudomonas aeruginosa WT", 
+    "-2212":"Pseudomonas aeruginosa antibiotic-resistant", 
+    "3122":"Streptococcus pneumoniae", 
+    "-3122":"Capsule-deficient streptococcus pneumoniae", 
+    "3312":"Mycobacterium tuberculosis", 
+    "-3312":"Drug-resistant mycobacterium tuberculosis", 
+    "3334":"Salmonella enterica", 
+    "-3334":"Salmonella mutant"
+}     #dictionary of all existing bacterial id
+
+
 def calculate_momentum(x,y,z): #defines the function to calculate momentum
     sqrd_values=x**2+y**2+z**2 #saves into a variable sum of squared momentum components
     try: 
@@ -11,11 +27,12 @@ def calculate_momentum(x,y,z): #defines the function to calculate momentum
     except: #if another error occurs warns the user about error
         print("An error occured")
 
-def clean_file(filename): #a function to clean dataset
-    cleaned_filename="output-Set0-cleaned.txt" #introduces name for cleaned file
+
+def clean_file(filename, bacterial_id): #a function to clean dataset
+    name, doc_ext=filename.rsplit(".", 1)
+    cleaned_filename=f"{name}_cleaned.{doc_ext}" #introduces name for cleaned file
     if os.path.exists(cleaned_filename): #check if cleaned version already exists
-        return "output-Set0-cleaned.txt" #refers user to it
-    bacterial_id=["211", "-211", "321", "-321", "2212", "-2212", "3122", "-3122", "3312", "-3312", "3334", "-3334"] #list of all existing bacterial id
+        return cleaned_filename #refers user to it
     incorrect_values=0 #counts how many data lines were removed
     with open(filename, "r") as file: #opens file for reading
         lines=file.readlines() #reads file lien by line and saves into a list
@@ -31,20 +48,24 @@ def clean_file(filename): #a function to clean dataset
     if incorrect_values>0: #checks if during cleaning any data was removed 
         with open(cleaned_filename, 'w') as final_file: #opens a new file
             final_file.writelines(clean_version) #saves cleaned data to new file
-        return "output-Set0-cleaned.txt" #returns it for further analyses
+        return cleaned_filename #returns it for further analyses
     else: #if no data was removed
         return filename #returns the original file for further analyses
 
-    
-print("--The momentum of each bacteria--\n") #prints the title
-filename=clean_file("output-Set0.txt") #saves the name of file that will be used in a variable
-with open(filename, "r") as file: #opens the file for reading
-    for i, line in enumerate(file): #iterates over each line in file, saves line index as "i" and line content as "line"
-        values=line.split() #splits the line string into separate values
-        if i==0: 
-            continue #skips the first line
-        x=float(values[0]) #saves x component as separate variable
-        y=float(values[1]) #saves y component as separate variable
-        z=float(values[2]) #saves z component as separate variable
-        momentum=calculate_momentum(x, y, z) #calls the function to calculate the momentum and saves the result in a variable
-        print(f"Bacteria No. {i} Momentum: {momentum}") #prints the calculated momentum & explains what's printed
+
+def bacterial_momentum():
+    print("--The momentum of each bacteria--\n") #prints the title
+    dataset=input("Enter the name of the file you want to work with:") #asks user for file name
+    filename=clean_file(dataset, bacterial_id) #saves the name of file that will be used in a variable
+    with open(filename, "r") as file: #opens the file for reading
+        for i, line in enumerate(file): #iterates over each line in file, saves line index as "i" and line content as "line"
+            values=line.split() #splits the line string into separate values
+            if i==0: 
+                continue #skips the first line
+            x=float(values[0]) #saves x component as separate variable
+            y=float(values[1]) #saves y component as separate variable
+            z=float(values[2]) #saves z component as separate variable
+            momentum=calculate_momentum(x, y, z) #calls the function to calculate the momentum and saves the result in a variable
+            print(f"Bacteria No. {i} Momentum: {momentum}") #prints the calculated momentum & explains what's printed
+
+bacterial_momentum()
---
+++ week3.py
@@ -0,0 +1,11 @@
+filename="output-Set7.txt"
+
+with open(filename, "r") as file:
+    bacteria=file.readlines()
+
+'''
+1. calculate the amount of each strain in each chunk
+2. calculate the average 
+
+output: average number of each strain
+'''
